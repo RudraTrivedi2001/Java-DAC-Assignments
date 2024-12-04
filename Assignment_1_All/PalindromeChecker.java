cd96fdf9932a8f6de0f18acef8df2ec8acf588a3
Aniruddha Prakash Trivedi <159725513+RudraTrivedi2001@users.noreply.github.com>
2024-12-04 09:21:53 -0800

    Update PalindromeChecker.java
    i had provided wrong information

--- Assignment_1_All/PalindromeChecker.java
+++ Assignment_1_All/PalindromeChecker.java
@@ -4,27 +4,30 @@ import java.util.Scanner;
 
 //Q8 palindrome checker
 
-public class PalindromeChecker {
-	public static void palindrome(int number) {
-		int temp=number;
-		int rem;
-		int check =0;
-		while(number!=0) {
-			rem=number %10;
-			check=(check*10)+rem;
-			number =number /10;
-		}
-		if(temp==check) System.out.println("palindrome number and number is "+temp);
-		else System.out.println(" !!not!! palindrome number and number is "+temp);
-	}
-	
-	
-	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		System.out.println("enter number you want to check ");
-		int num=s.nextInt();
-		palindrome(num);
-	}
-	
-	
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input from user
+        System.out.print("Enter a string to check for palindrome: ");
+        String input = scanner.nextLine();
+
+        // Convert the string to lowercase to make it case-insensitive
+        String lowerCaseInput = input.toLowerCase();
+
+        // Reverse the string
+        String reversed = "";
+        for (int i = lowerCaseInput.length() - 1; i >= 0; i--) {
+            reversed += lowerCaseInput.charAt(i);
+        }
+
+        // Check if the original and reversed strings are the same
+        if (lowerCaseInput.equals(reversed)) {
+            System.out.println("The string \"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is not a palindrome.");
+        }
+
+        scanner.close();
+    }
 }
